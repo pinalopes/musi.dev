95d35d8d066c1a925a807a944b8b05859aba2753
pinalopes <lab>
2024-07-03 19:50:09 -0300

    projeto interdisciplinar

---
+++ Semestre1/Projeto interdisciplinar/PI.py
@@ -0,0 +1,33 @@
+lista=[]
+print(
+    """===CONVERSOR DE BASES==="""
+)
+n=int(input('Digite o número: '))
+print(""""
+[1]BINÁRIO
+[2]HEXADECIMAL
+[3]OCTAL
+""")
+opção=int(input('Digite o número da opção desejada: '))
+if opção==1:
+    while n>=2:
+        resto=n%2
+        divisao=n//2
+        n=divisao
+        lista.insert(0,resto)
+        print(divisao,lista)
+elif opção==2:
+    numeros_decimais='0123456789ABCDEF'
+    while n>0:
+        resto=n%16
+        divisao=n//16
+        n=divisao
+        lista.insert(0, numeros_decimais[resto])
+        print(divisao,lista)
+elif opção==3:
+    while n >=8:
+        resto=n%8
+        divisao=n//8
+        n=divisao
+        lista.insert(0,resto)
+        print(divisao,lista)
