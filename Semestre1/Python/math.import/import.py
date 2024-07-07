b4b5a7d23155452cb5f6f0684708dbd30687d14a
pinalopes <lab>
2024-07-07 19:42:19 -0300

    semestre1

---
+++ Semestre1/Python/math.import/import.py
@@ -0,0 +1,37 @@
+import math
+print(math.ceil(4.2))#Retorna o menor inteiro maior ou igual a x.
+print(math.floor(4.7))# Retorna o maior inteiro menor ou igual a x.
+print(math.trunc(4.9))# Retorna a parte inteira de x, eliminando a parte fracionária.
+print(math.fabs(-5.5))#Retorna o valor absoluto de x.
+print(math.factorial(5))# Retorna o fatorial de x.
+print(math.fmod(7, 3))#Retorna o resto da divisão de x por y.
+print(math.exp(2))#Retorna e elevado à potência x.
+print(math.log(100,10))#Retorna o logaritmo de x na base fornecida (ou logaritmo natural se a base não for especificada).
+print(math.log2(8))#Retorna o logaritmo de base 2 de x.
+print(math.log10(1000))#Retorna o logaritmo de base 10 de x.
+print(math.pow(2, 3))# Retorna x elevado à potência y.
+print(math.sqrt(16))#Retorna a raiz quadrada de x.
+print(math.cos(4))# Retorna o cosseno de x.
+print(math.sin(2))# Retorna o seno de x.
+print(math.tan(4))#Retorna a tangente de x.
+print(math.acos(0.5))# Retorna o arco cosseno de x.
+print(math.asin(0.5))# Retorna o arco seno de x.
+print(math.atan(1))#Retorna o arco tangente de x.
+print(math.atan2(1, 1))#Retorna atan(y / x), levando em conta os sinais de ambos os argumentos para determinar o quadrante correto do resultado.
+print(math.cosh(0))# Retorna o cosseno hiperbólico de x.
+print(math.sinh(0))# Retorna o seno hiperbólico de x.
+print(math.tanh(0))#Retorna a tangente hiperbólica de x.
+print(math.acosh(1))# Retorna o arco cosseno hiperbólico de x.
+print(math.asinh(0))# Retorna o arco seno hiperbólico de x.
+print(math.atanh(0))# Retorna o arco tangente hiperbólico de x.
+print(math.degrees(math.pi))# Converte x de radianos para graus.
+print(math.radians(180))#Converte x de graus para radianos.
+print(math.isfinite(10))# Retorna True se x é um número finito.
+print(math.isinf(math.inf))#Retorna True se x é infinito.
+print(math.isnan(math.nan)) #Retorna True se x não é um número (NaN).
+print(math.isclose(1.0000001,10000002)) #Retorna True se os valores a e b são aproximadamente iguais, com base nas tolerâncias relativa e absoluta fornecidas.
+print(math.pi) #A constante π (pi).
+print(math.e) #A constante e (base dos logaritmos naturais).
+print(math.tau) #A constante τ (tau), que é 2π.
+print(math.inf )#Representação de infinito positivo.
+print(math.nan )#Representação de um valor NaN (Not a Number)
